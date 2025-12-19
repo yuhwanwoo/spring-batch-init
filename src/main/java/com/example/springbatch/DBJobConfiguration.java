e5689552a3fc5a39f46e073847fe901af9de461e
yuhwanwoo <6514687@naver.com>
2025-12-19 16:28:43 +0900

    테이블 찾지 못할 떄 에러 예시

---
+++ src/main/java/com/example/springbatch/DBJobConfiguration.java
@@ -0,0 +1,54 @@
+package com.example.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class DBJobConfiguration {
+
+    @Bean
+    public Job helloJob(JobRepository jobRepository, Step step1, Step step2, Step step3) {
+        return new JobBuilder("Job", jobRepository) // Factory 대신 직접 생성
+                .start(step1)
+                .next(step2)
+                .next(step3)
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1 has executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager) // TransactionManager 필수
+                .build();
+    }
+
+    @Bean
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 has executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step3(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step3", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step3 has executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}
