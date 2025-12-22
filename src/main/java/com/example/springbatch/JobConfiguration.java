494fb4006bb35faf16b9c60455cc99f7cdc8452d
yuhwanwoo <6514687@naver.com>
2025-12-22 11:57:40 +0900

    기본 잡 변경

--- src/main/java/com/example/springbatch/JobConfiguration.java
+++ src/main/java/com/example/springbatch/JobConfiguration.java
@@ -11,44 +11,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-public class DBJobConfiguration {
+public class JobConfiguration {
 
     @Bean
-    public Job dbjob(JobRepository jobRepository, Step dbStep1, Step dbStep2, Step dbStep3) {
-        return new JobBuilder("DbJob", jobRepository) // Factory 대신 직접 생성
-                .start(dbStep1)
-                .next(dbStep2)
-                .next(dbStep3)
+    public Job job(JobRepository jobRepository, Step step1, Step step2) {
+        return new JobBuilder("Job", jobRepository)
+                .start(step1)
+                .next(step2)
                 .build();
     }
 
     @Bean
-    public Step dbStep1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("step1", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step1 has executed");
                     return RepeatStatus.FINISHED;
-                }, transactionManager) // TransactionManager 필수
+                }, transactionManager) // transactionManager 명시 필수
                 .build();
     }
 
     @Bean
-    public Step dbStep2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("step2", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step2 has executed");
                     return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
-    }
-
-    @Bean
-    public Step dbStep3(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step3", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step3 has executed");
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
+                }, transactionManager) // transactionManager 명시 필수
                 .build();
     }
 }
