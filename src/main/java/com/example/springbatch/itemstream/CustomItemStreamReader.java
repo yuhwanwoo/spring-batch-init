f7e7e30f1d9cfa1f4ad5b642a47f78833aeab96d
yuhwanwoo <6514687@naver.com>
2026-03-23 09:46:45 +0900

    custom item stream reader 추가

---
+++ src/main/java/com/example/springbatch/itemstream/CustomItemStreamReader.java
@@ -0,0 +1,57 @@
+package com.example.springbatch.itemstream;
+
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.batch.item.ItemStreamReader;
+
+import java.util.List;
+
+public class CustomItemStreamReader implements ItemStreamReader<String> {
+
+    private final List<String> items;
+    private int index = -1;
+    private boolean restart = false;
+
+    public CustomItemStreamReader(List<String> items) {
+        this.items = items;
+        this.index = 0;
+    }
+
+    @Override
+    public String read() throws Exception {
+        String item = null;
+
+        if(this.index < this.items.size()) {
+            item = this.items.get(index);
+            index++;
+        }
+
+        if(this.index == 6 && !restart) {
+            throw new RuntimeException("Restart is required.");
+        }
+
+        return item;
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        if(executionContext.containsKey("index")) {
+            index = executionContext.getInt("index");
+            this.restart = true;
+        }
+        else {
+            index = 0;
+            executionContext.put("index", index);
+        }
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+        executionContext.put("index", index);
+    }
+
+    @Override
+    public void close() throws ItemStreamException {
+        System.out.println("close");
+    }
+}
