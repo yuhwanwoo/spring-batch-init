a9e92ce76554e4a69ca8bece20e39c6db165a331
yuhwanwoo <6514687@naver.com>
2026-03-27 21:14:30 +0900

    custom reader writer 구현

---
+++ src/main/java/com/example/springbatch/chunkprocessor/CustomItemReader.java
@@ -0,0 +1,23 @@
+package com.example.springbatch.chunkprocessor;
+
+import org.springframework.batch.item.ItemReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomItemReader implements ItemReader<Customer> {
+
+    private List<Customer> list;
+
+    public CustomItemReader(List<Customer> list) {
+        this.list = new ArrayList<>(list);
+    }
+
+    @Override
+    public Customer read() {
+        if (!list.isEmpty()) {
+            return list.remove(0);
+        }
+        return null;
+    }
+}
