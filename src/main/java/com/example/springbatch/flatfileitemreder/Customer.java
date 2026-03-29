e0cc702befd43714841eefcf6017f94a4c12642f
yuhwanwoo <6514687@naver.com>
2026-03-29 11:59:25 +0900

    custom fileset mapper

---
+++ src/main/java/com/example/springbatch/flatfileitemreder/Customer.java
@@ -0,0 +1,12 @@
+package com.example.springbatch.flatfileitemreder;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private String name;
+    private int age;
+    private String year;
+
+}
