a21821f2a1a17a251cbe24630a00a6148f1e51a9
yuhwanwoo <6514687@naver.com>
2026-04-20 20:28:22 +0900

    processor 추가

---
+++ src/main/java/com/example/springbatch/skip/SkipItemProcessor.java
@@ -0,0 +1,22 @@
+package com.example.springbatch.skip;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class SkipItemProcessor implements ItemProcessor<String, String> {
+
+	private int cnt = 0;
+
+	@Override
+	public String process(String item) throws Exception {
+
+		if(item.equals("6") || item.equals("7")) {
+			System.out.println("ItemProcessor : " + item);
+			cnt++;
+			throw new SkippableException("Process failed. cnt:" + cnt);
+		}
+		else {
+			System.out.println("ItemProcessor : " + item);
+			return String.valueOf(Integer.valueOf(item) * -1);
+		}
+	}
+}
