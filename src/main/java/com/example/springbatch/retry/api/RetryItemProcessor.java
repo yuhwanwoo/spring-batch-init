2fc82d57f443c5c83a62bb4f72d5a9c9ab6de0f8
yuhwanwoo <6514687@naver.com>
2026-04-24 16:18:27 +0900

    api processor 추가

---
+++ src/main/java/com/example/springbatch/retry/api/RetryItemProcessor.java
@@ -0,0 +1,18 @@
+package com.example.springbatch.retry.api;
+
+import com.example.springbatch.retry.NoRetryException;
+import org.springframework.batch.item.ItemProcessor;
+
+public class RetryItemProcessor implements ItemProcessor<String, String> {
+
+	private int cnt = 0;
+
+	@Override
+	public String process(String item) throws Exception {
+		if(item.equals("2") || item.equals("3")) {
+			cnt++;
+			throw new NoRetryException("Process failed. cnt:" + cnt);
+		}
+		return item;
+	}
+}
