8394c23e0b2b7a9618b06d7639b62dd7c1af9d27
yuhwanwoo <6514687@naver.com>
2026-04-25 19:53:37 +0900

    writer add/ fg

---
+++ src/main/java/com/example/springbatch/retry/api/Customer.java
@@ -0,0 +1,14 @@
+package com.example.springbatch.retry.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Michael Minella
+ */
+@Data
+@AllArgsConstructor
+public class Customer {
+
+	private String item;
+}
