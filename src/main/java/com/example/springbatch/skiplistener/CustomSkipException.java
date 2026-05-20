747841118818027b896d1c42b68a798957304f8b
yuhwanwoo <6514687@naver.com>
2026-05-20 22:24:04 +0900

    skip listener

---
+++ src/main/java/com/example/springbatch/skiplistener/CustomSkipException.java
@@ -0,0 +1,12 @@
+package com.example.springbatch.skiplistener;
+
+public class CustomSkipException extends Exception {
+
+    public CustomSkipException() {
+        super();
+    }
+
+    public CustomSkipException(String message) {
+        super(message);
+    }
+}
