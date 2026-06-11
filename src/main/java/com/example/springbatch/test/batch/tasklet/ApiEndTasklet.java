c115d1319648a7c3bb47d53bfee62ec1814d0568
yuhwanwoo <6514687@naver.com>
2026-06-11 18:29:14 +0900

    tasklet add

---
+++ src/main/java/com/example/springbatch/test/batch/tasklet/ApiEndTasklet.java
@@ -0,0 +1,25 @@
+package com.example.springbatch.test.batch.tasklet;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiEndTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+        System.out.println("");
+        System.out.println(">> ApiEndTasklet is started");
+        System.out.println("");
+        System.out.println("******************************************************************************************************************************************************");
+        System.out.println("*                                                               Spring Batch is completed                                                            *");
+        System.out.println("******************************************************************************************************************************************************");
+        System.out.println("");
+
+        return RepeatStatus.FINISHED;
+    }
+}
---
+++ src/main/java/com/example/springbatch/test/batch/tasklet/ApiStartTasklet.java
@@ -0,0 +1,21 @@
+package com.example.springbatch.test.batch.tasklet;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiStartTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+        System.out.println("");
+        System.out.println(">> ApiStartTasklet is started");
+        System.out.println("");
+
+        return RepeatStatus.FINISHED;
+    }
+}
